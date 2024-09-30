845044d84ee16490cff84a2b374869e261e35e6d
Lars Kellogg-Stedman <lars@oddbit.com>
2024-09-30 18:18:47 -0400

    Caching and output improvements

--- geolocate.py
+++ geolocate.py
@@ -1,3 +1,9 @@
+from typing import Any, cast
+
+import os
+import sys
+import io
+import diskcache
 import click
 import dotenv
 import requests
@@ -15,11 +21,10 @@ from rich import box
 from rich.console import Console
 from rich.table import Table
 
-from fccdb import Base
 from fccdb import Entity
-from fccdb import Amateur
+from fccdb import History
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, RootModel
 
 dotenv.load_dotenv()
 LOG = logging.getLogger(__name__)
@@ -45,11 +50,11 @@ LOG = logging.getLogger(__name__)
 # ]
 
 
-class LocatorApiResponse(BaseModel):
+class LocatorApiResult(BaseModel):
     place_id: int
     licence: str
-    osm_type: str
-    osm_id: int
+    osm_type: str | None = None
+    osm_id: int | None = None
     boundingbox: tuple[str, str, str, str]
     lat: str
     lon: str
@@ -59,6 +64,9 @@ class LocatorApiResponse(BaseModel):
     importance: float
 
 
+LocatorApiResponse = RootModel[list[LocatorApiResult]]
+
+
 class Location(BaseModel):
     lat: float
     lon: float
@@ -66,12 +74,24 @@ class Location(BaseModel):
 
 class Locator:
     geocode_url = "https://geocode.maps.co/search?q={address}&api_key={api_key}"
+    cache: dict[str, Any] = {}
+    cache_hits: int = 0
+    cache_misses: int = 0
 
-    def __init__(self, apikey: str):
+    def __init__(self, apikey: str, cachepath: str | None = None):
         self.apikey = apikey
         self.last_request = time.time()
 
-    def locate(self, address: str) -> Location:
+        if cachepath is None:
+            xdg_cache_dir = os.environ.get(
+                "XDG_CACHE_HOME", os.path.join(os.environ["HOME"], ".cache")
+            )
+
+            cachepath = os.path.join(xdg_cache_dir, "fccdb")
+
+        self.cache = diskcache.Cache(cachepath)
+
+    def lookup_address(self, address: str) -> LocatorApiResponse:
         now = time.time()
         if (now - self.last_request) < 1:
             time.sleep(1)
@@ -79,22 +99,45 @@ class Locator:
         res = requests.get(
             self.geocode_url.format(api_key=self.apikey, address=urlquote(address))
         )
-        res.raise_for_status()
-        obj: list[dict[str, str]] = res.json()
+        return LocatorApiResponse.model_validate(res.json())
 
-        if len(obj) > 1:
+    def locate(self, address: str) -> Location:
+        if address in self.cache:
+            self.cache_hits += 1
+            return self.cache[address]
+
+        self.cache_misses += 1
+        res = self.lookup_address(address)
+
+        if len(res.root) > 1:
             raise ValueError(f"multiple results for {address}")
-        if len(obj) == 0:
+        if len(res.root) == 0:
             raise ValueError(f"no results for {address}")
-        loc = LocatorApiResponse.model_validate(obj[0])
-        return Location(lat=float(loc.lat), lon=float(loc.lon))
+
+        loc = Location(lat=float(res.root[0].lat), lon=float(res.root[0].lon))
+        self.cache[address] = loc
+        return loc
 
 
 @click.command(context_settings={"auto_envvar_prefix": "FCC"})
 @click.option("--dburi", "-d")
 @click.option("--api-key", "-k")
 @click.option("--verbosity", "-v", count=True)
-def main(dburi: str, api_key: str, verbosity: int):
+@click.option(
+    "--label-format", "-L", default="{full_name} [{operator_class}] {call_sign}"
+)
+@click.option("--desc-format", "-D", default="{address}")
+@click.option(
+    "--output", "-o", "output_file", type=click.File(mode="w"), default=sys.stdout
+)
+def main(
+    dburi: str,
+    api_key: str,
+    verbosity: int,
+    label_format: str,
+    desc_format: str,
+    output_file: io.IOBase,
+):
     logLevel = ["WARNING", "INFO", "DEBUG"][min(verbosity, 2)]
     logging.basicConfig(
         level=logLevel,
@@ -111,7 +154,7 @@ def main(dburi: str, api_key: str, verbosity: int):
             last_status: History | None = next(
                 reversed(sorted(entity.history, key=lambda x: x.log_date)), None
             )
-            name = " ".join(
+            full_name = " ".join(
                 [
                     getattr(entity, x)
                     for x in ["first_name", "mi", "last_name"]
@@ -122,7 +165,7 @@ def main(dburi: str, api_key: str, verbosity: int):
                 "LICAN",
                 "LIEXP",
             ]:
-                LOG.info(f"skipping {name} ({entity.call_sign}): license expired")
+                LOG.info(f"skipping {full_name} ({entity.call_sign}): license expired")
             address = f"{entity.street_address}, {entity.city}, {entity.state} {entity.zip_code}"
             try:
                 loc = locator.locate(address)
@@ -130,16 +173,28 @@ def main(dburi: str, api_key: str, verbosity: int):
                 LOG.error(err)
                 continue
 
+            attrs = {
+                "full_name": full_name,
+                "loc": loc,
+                "address": address,
+            }
+            attrs.update(entity.to_dict())
+            attrs.update(entity.license.to_dict())
+
+            desc = desc_format.format(**attrs)
+            label = label_format.format(**attrs)
+
             gpxout.wpt.append(
                 gpx.Waypoint(
-                    name=f"{name} [{entity.license.operator_class}]",
-                    desc=address,
+                    name=label,
+                    desc=desc,
                     lat=loc.lat,
                     lon=loc.lon,
                 )
             )
 
-    print(gpxout.to_xml(skip_empty=True, pretty_print=True).decode())
+    with output_file:
+        output_file.write(gpxout.to_xml(skip_empty=True, pretty_print=True).decode())
 
 
 if __name__ == "__main__":
